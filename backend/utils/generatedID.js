d9f96cf252bb5ba25a0f23e5131370bebee02788
kanis-2129 <kaniswarik@gmail.com>
2025-12-24 21:11:34 +0530

    Delete backend/utils directory

--- backend/utils/generatedID.js
+++
@@ -1,7 +0,0 @@
-export const generateCustomerId = (latestNumber) => {
-  return `CUST-${String(latestNumber).padStart(4, "0")}`;
-};
-
-export const generateOrderId = (latestNumber) => {
-  return `ORD-${String(latestNumber).padStart(4, "0")}`;
-};
